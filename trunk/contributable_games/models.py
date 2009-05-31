caccdf0145003ddd8684546dc84605a41d91f80b
gnubis <gnubis@53f65040-4875-11de-b695-a305669b6508>
2009-05-31 22:33:27 +0000

    First commit

---
+++ trunk/contributable_games/models.py
@@ -0,0 +1,13 @@
+#-Input info about your open house:
+#    -Place (geo-location with map)
+#    -Date
+#    -Text description (300 chars max)
+#    -Stuff to gather (some required, some not)
+#    -Min people to be confirmed
+#    -Max people allowed
+#    -Guestlist:
+#        -some can append to the guestlist
+#        
+#
+
+from django.db import Models
