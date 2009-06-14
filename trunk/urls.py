67d7622115c570450c4d698eea36f5f59d7d36fe
gnubis <gnubis@53f65040-4875-11de-b695-a305669b6508>
2009-06-14 00:19:50 +0000

    ahi van algunos archivos y el urls nuevo

--- trunk/urls.py
+++ trunk/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
     (r'static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     
-    #/main/* urls internas
+    #urls internas
     url(r'^register/$', register, name='register'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login',
         {'template_name': 'contributable_games/login.html'}, name='login'),
