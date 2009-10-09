7f2bbf83aa62ab8f395a8bc78b7ee57facf786dc
gnubis <gnubis@53f65040-4875-11de-b695-a305669b6508>
2009-10-09 03:38:18 +0000

    funcionando como antes los juegos pero con archivos estaticos

--- trunk/contributable_games/unzip.py
+++ trunk/contributable_games/unzip.py
@@ -5,30 +5,11 @@ import os.path
 import getopt
 
 def unzip(zf, dir):
-    def _makedirs(directories, basedir):
-        for dir in directories:
-            curdir = os.path.join(basedir, dir)
-            if not os.path.exists(curdir):
-                os.mkdir(curdir)
-
-    def _listdirs():
-        dirs = []
-        for name in zf.namelist():
-            if name.endswith('/'):
-                dirs.append(name)
-        dirs.sort()
-        return dirs
-    
-    def _createstructure(dir):
-        _makedirs(_listdirs(), dir)
-    
-    if not dir.endswith(':') and not os.path.exists(dir):
-        os.mkdir(dir)
-
-    _createstructure(dir)
-
     for i, name in enumerate(zf.namelist()):
         if not name.endswith('/'):
+            dirname = os.path.dirname(os.path.join(dir, name))
+            if not os.path.isdir(dirname):
+                os.makedirs(dirname)
             outfile = open(os.path.join(dir, name), 'wb')
             outfile.write(zf.read(name))
             outfile.flush()
