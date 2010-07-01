9978cb8eb1004f77efbb592036291dbc2845cb49
meliascosta <meliascosta@53f65040-4875-11de-b695-a305669b6508>
2010-07-01 22:41:48 +0000

    add granprix

---
+++ static/games/granpri/res/checkpoint.js
@@ -0,0 +1,71 @@
+
+function Checkpoint(trials,on_end,on_check){
+	$("body").css({
+		"text-align": 'center',
+		"margin": '0 auto'
+	})
+	$("body").append("<div id=check-holder style=\"position: absolute; height: 3em; width: 98%; top: 1%; left: 0%;background-color:#2E81FF;-moz-border-radius:5px;-webkit-border-radius:5px;\"></div>");
+	var IMG_URL = './res/images/checkpoints/',
+		$chdiv = $("#check-holder"),
+		trials = trials,
+		TRIALS = trials.slice(0),
+		on_end = on_end,
+		on_check = on_check;
+		N = trials.length,
+		ntrials = trials[N-1],
+		curr_trial = 0,
+		curr_checkpoint = 1,
+		fallback = 0;
+		
+	$("body").append("<div id=text class=messages style=\"position: absolute;left:20%; width:60%;bottom:0; color: #C79EE1;font-weight:bold;font-size:2em;text-align:center;border:solid;-moz-border-radius:10px;-webkit-border-radius:10px;background-color:#481369;\"><p>PEPE</p></div>")
+	$("#text").hide();
+	
+	for(i=0;i<N;i++){
+		$($chdiv).append("<div id=checkpoint"+ i +" class=checkpoint style=\"position: absolute; height: 100%; width: 5%; top: 0%; left: "+Math.round(trials[i]*93/ntrials)+"%;background-color: white\"></div>");
+	}
+	$($chdiv).append("<div id=slider style=\"position: absolute; height: 100%; width: 5%; top: 0%; left: 0%;background-color: grey;\"></div>");
+	for(i=0;i<=N;i++){
+	$("#slider").append("<img id=\"checkimg"+i+"\" src=\""+IMG_URL+(i+1)+".png\" style=\" width:100%;height:100%;left:0%; \">")		
+	}
+
+	$("#slider > img").not("#checkimg0").hide();
+	
+	$($chdiv).append("<div id=moving-bar style=\"position: absolute; height: 100%; width: 0%; top: 0%; left: 0%;background-color: red;opacity:0.5; \"></div>");
+	var $mv = $("#moving-bar"),
+		$sl = $("#slider"),
+	    $sl_imgs = $("#slider > img");
+		
+	this.step = function(){ // advance bar one unit if it reaches a checkpoint change image and slide it.
+		curr_trial++;
+		$($mv).animate({width: Math.round(curr_trial/ntrials*93) +"%"}, 500 );
+		
+		if (curr_trial== trials[0]){
+			curr_checkpoint++;
+			fallback = parseInt(trials.splice(0,1));
+			$($sl_imgs).not("#checkimg"+(curr_checkpoint-1)).hide();
+			$("#checkimg"+(curr_checkpoint-1)).show();
+			setTimeout(function(){$($sl).animate({left: Math.round(curr_trial/ntrials*93) +"%"},500,function(){if (trials.length == 0){ on_end();}else{on_check();}})},500);
+		}
+	}
+	this.fallback = function(){ //fallback to last checkpoint
+		$($mv).animate({width: Math.round(fallback/ntrials*93)+"%"}, 500 );
+		curr_trial = fallback;	
+	}
+	this.set_at = function(checkN){ // set everything to be at checkpoint number checkN
+		trials = TRIALS.slice(0);
+		if (checkN>0){temp = trials[checkN-1]; trials.splice(0,checkN);}else{temp=0}
+		$($mv).css("width", Math.round(temp/ntrials*93)+"%");
+		$($sl_imgs).not("#checkimg"+checkN).hide();
+		$("#checkimg"+(checkN)).show();
+		$($sl).css("left", Math.round(temp/ntrials*93)+ "%");
+		curr_trial = temp;
+		fallback = temp;
+		curr_checkpoint = checkN+1;
+	}
+	this.displaytext = function(text){
+		$("#text > p").text(text);
+		$("#text").slideDown();
+		setTimeout(function(){$("#text").fadeOut('fast')},3000);
+	}
+	
+}
---
+++ static/games/granpri/res/highscore.js
@@ -0,0 +1,38 @@
+
+function Highscore(new_game){
+	var new_game=new_game;
+	$("body").append("<div id=highscore style=\"position: absolute; text-align:center;height: 50%; width: 500px; top: 50%; left: 50%;margin-left:-250px;margin-top:-15%;background-color:transparent\"></div>");
+	var IMG_URL = './res/images/';
+		
+	$("#highscore").hide();	
+	$("#highscore").append("<table id=htable style=\"color:white;table-layout:fixed;width:500px; text-align:center;margin-left: auto; margin-right: auto; background-color:brown; \"><thead style=\"background-color:black\"><tr><th>AVENTURERO</th><th>BOTIN</th></tr></thead></table>");
+	for(i=0;i<10;i++){
+		$("#htable").append("<tr><td><p>...</p></td><td><p>...</p></td></tr>");
+	}
+	$("#htable").append("<tr style=\"background-color:#E49213\"><td colspan= 2><p id=puntos>TUS PUNTOS: </p></td></tr>");
+	$("#htable").append("<tr style=\"background-color:red\"><td id=nuevo style=\"cursor:pointer;cursor:hand;\"><p>NUEVO</p></td><td id=\"exit\"><a href= \"http://juegos.df.uba.ar/\">SALIR</a></td></tr>");
+	$("#nuevo").bind("click",function(){
+		$("#highscore").hide();	
+		new_game();
+	});
+	this.showTable = function(puntos){ // Show the Highscores table
+		var scores = $.games.get_highscores();
+		for ( i=0;i<scores.length;i++){
+				$("#htable td:even:eq("+i+") p").text(scores[i].name)
+											.css("background-color","#9B8677");
+
+				$("#htable td:odd:eq("+i+") p").text(scores[i].score+" keV")
+											.css("background-color","white")
+											.css("color","black");
+			}			
+		$("#puntos").text("TUS PUNTOS: "+puntos+" keV");
+		$("#highscore").slideDown();		
+		
+	}
+	
+	this.hideTable = function(){ 
+		$("#highscore").hide();	
+	}
+	
+	
+}
