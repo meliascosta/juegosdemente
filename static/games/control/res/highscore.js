10058a661a1a07af200c9bfdf2298d54e2af4c39
meliascosta <meliascosta@53f65040-4875-11de-b695-a305669b6508>
2010-09-09 16:11:00 +0000

    cambios en los highscore

--- static/games/control/res/highscore.js
+++ static/games/control/res/highscore.js
@@ -10,14 +10,14 @@ function Highscore(new_game){
 		$("#htable").append("<tr><td><p>...</p></td><td><p>...</p></td></tr>");
 	}
 	$("#htable").append("<tr style=\"background-color:#E49213\"><td colspan= 2><p id=puntos>TUS PUNTOS: </p></td></tr>");
-	$("#htable").append("<tr style=\"background-color:red\"><td id=nuevo style=\"cursor:pointer;cursor:hand;\"><p>NUEVO</p></td><td id=\"exit\"><a href= \"http://juegos.df.uba.ar/\">SALIR</a></td></tr>");
+	$("#htable").append("<tr style=\"background-color:red\"><td id=nuevo style=\"cursor:pointer;cursor:hand;\"><p>NUEVO</p></td><td id=\"exit\"><a href= \""+$.games.main_site+"\">SALIR</a></td></tr>");
 	$("#nuevo").bind("click",function(){
 		$("#highscore").hide();	
 		new_game();
 	});
 	this.showTable = function(puntos){ // Show the Highscores table
 		var scores = $.games.get_highscores();
-		for ( i=0;i<scores.length;i++){
+		for ( i=0;i<(scores.length<10?scores.length:10);i++){
 				$("#htable td:even:eq("+i+") p").text(scores[i].name)
 											.css("background-color","#9B8677");
 
